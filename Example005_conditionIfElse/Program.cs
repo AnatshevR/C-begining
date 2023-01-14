9d404d667e20120e08f9b4a338bcf5de565af3c6
Roman_Anatshev <romananatshev@gmail.com>
2023-01-15 00:56:54 +0300

    Example 005 condition if else

---
+++ Example005_conditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Yehooo, this is Masha");
+}
+else 
+{
+    Console.WriteLine("Hello");
+    Console.WriteLine(username);
+}
